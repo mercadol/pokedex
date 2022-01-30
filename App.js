384b37e80e3ddaad77d3653c7449c4395f57dfc2
Leonardo Mercado <lamercadob1@gmail.com>
2022-01-30 15:39:54 -0500

    01/17 Curso practico de React Native

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>{/* Rest of your app code 
+      <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      </View>
+    */}</NavigationContainer>
   );
 }
 
